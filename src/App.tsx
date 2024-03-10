7917cc57c09bba9a716cfc0b948c8af9e703b5b2
shahin2525 <shahin451999@gmail.com>
2024-03-09 23:16:33 -0800

    antd layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import MainLayout from "./components/layout/MainLayout";
 
 function App() {
   return (
     <>
-      <h1>PH university management</h1>
+      <MainLayout></MainLayout>
     </>
   );
 }
