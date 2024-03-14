58b04edd15bf62b46fd6d0305ed48db1e029f461
shahin2525 <shahin451999@gmail.com>
2024-03-14 16:29:11 -0700

    admin sidebar added by programatic way

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,32 +1,33 @@
 import { Layout, Menu, MenuProps } from "antd";
 import { NavLink, Outlet } from "react-router-dom";
+import { adminSidebarItems } from "../../routes/admin.router";
 const { Header, Content, Footer, Sider } = Layout;
 
-const items: MenuProps["items"] = [
-  {
-    key: "Dashboard",
-    label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
-  },
+// const items: MenuProps["items"] = [
+//   {
+//     key: "Dashboard",
+//     label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
+//   },
 
-  {
-    key: "User Management",
-    label: "User Management",
-    children: [
-      {
-        key: "Create-Admin",
-        label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
-      },
-      {
-        key: "Create-Faculty",
-        label: <NavLink to="/admin/create-faculty">Crate Faculty</NavLink>,
-      },
-      {
-        key: "Create-student",
-        label: <NavLink to="/admin/create-student">Create Student</NavLink>,
-      },
-    ],
-  },
-];
+//   {
+//     key: "User Management",
+//     label: "User Management",
+//     children: [
+//       {
+//         key: "Create-Admin",
+//         label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
+//       },
+//       {
+//         key: "Create-Faculty",
+//         label: <NavLink to="/admin/create-faculty">Crate Faculty</NavLink>,
+//       },
+//       {
+//         key: "Create-student",
+//         label: <NavLink to="/admin/create-student">Create Student</NavLink>,
+//       },
+//     ],
+//   },
+// ];
 
 const MainLayout = () => {
   return (
@@ -58,7 +59,7 @@ const MainLayout = () => {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["4"]}
-          items={items}
+          items={adminSidebarItems}
         />
       </Sider>
       <Layout style={{ height: "100vh" }}>
