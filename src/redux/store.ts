86903ba180e16eda005e2b1228e5baec1f3e8295
shahin2525 <shahin451999@gmail.com>
2024-03-17 13:38:33 -0700

    redux persist added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "./feature/auth/authSlice";
+import authReducer from "./feature/auth/authSlice";
 import { baseApi } from "./api/baseApi";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authSlice,
+    auth: persistedAuthReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
