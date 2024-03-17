f5805e831d95f93089818a467926f8d10b4ff36c
shahin2525 <shahin451999@gmail.com>
2024-03-17 11:14:47 -0700

    auth api added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,39 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/feature/auth/authApi";
+
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0002",
+      password: "admin123",
+    },
+  });
+  const [login, { data, error }] = useLoginMutation();
+  console.log("data=>", data);
+  console.log("error=>", error);
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    login(userInfo);
+  };
+
   return (
-    <div>
-      <h1>Login from </h1>
-    </div>
+    <form className="m-5" onSubmit={handleSubmit(onSubmit)}>
+      <div className="mt-2">
+        <label htmlFor="id">Id:</label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+
+      <div className="m-4">
+        <label htmlFor="password">Password:</label>
+        <input id="password" {...register("password")} />
+      </div>
+
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./feature/auth/authSlice";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
