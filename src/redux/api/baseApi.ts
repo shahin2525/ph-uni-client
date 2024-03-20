a0f84352049771d6944943081fcb4f6e591cfb23
shahin2525 <shahin451999@gmail.com>
2024-03-20 15:55:05 -0700

    refresh token has generated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -18,13 +18,15 @@ const baseQuery = fetchBaseQuery({
 
 const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
   const result = await baseQuery(args, api, extraOptions);
-  if (result.error?.status === 401) {
+  console.log(result);
+  if (result.error?.status == 401) {
     console.log("sending refresh token");
     const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
       method: "POST",
       credentials: "include",
     });
     const data = await res.json();
+    console.log(data);
     const user = (api.getState() as RootState).auth.user;
     api.dispatch(
       setUser({
