f2dbc43dd6912b3ccac8a9023085048303123f98
shahin2525 <shahin451999@gmail.com>
2024-04-07 10:54:29 -0700

    flex added

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,17 +1,22 @@
 import { FieldValues } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
 
 const CreateAcademicSemester = () => {
-  const onSubmit = (data): FieldValues => {
+  const onSubmit = (data: FieldValues) => {
     console.log(data);
   };
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" />
-      <Button htmlType="submit">submit</Button>
-    </PHForm>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="name" label="name" />
+          <PHInput type="text" name="year" label="year" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
